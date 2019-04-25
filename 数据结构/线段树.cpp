219236ccb5f2765d4dcc15c5c5e25fc5aee490c6
Congrool <783928876@qq.com>
2019-04-25 14:07:57 +0800

    Update 线段树.cpp
    重写了线段树模板，并通过测试

--- 数据结构/线段树.cpp
+++ 数据结构/线段树.cpp
@@ -1,101 +1,57 @@
+//成段增减区间求和线段树模板
 #include<iostream>
-#include<vector>
 #include<algorithm>
-#include<queue>
 #include<memory.h>
-#include<cstdlib>
-#include<ctime>
+#include<cstdio>
 using namespace std;
-const int MAXN = 30;
-const int INF = 0x3f3f3f3f;
-int arr[MAXN+1];
-int segt[(MAXN+1)<<2];
-int addmark[(MAXN+1)<<2]; //记录延迟标记
-void build(int node,int l,int r){
-    addmark[node] = 0;
-    if(l == r){
-        segt[node] = arr[l];
+typedef long long ll;
+const ll MAXN = 100010;
+ll arr[MAXN];
+ll segt[MAXN<<2],tag[MAXN<<2];
+ll N,Q;
+inline void push_up(ll p){
+//求和
+    segt[p] = segt[p<<1]+segt[(p<<1)|1];
+}
+void build(ll p,ll b,ll e){
+    tag[p] = 0;
+    if(b == e){
+        segt[p] = arr[b];
         return;
     }
-    build(node*2,l,l+(r-l)/2);
-    build(node*2+1,l+(r-l)/2+1,r);
-
-    if(segt[node*2] < segt[node*2+1])
-        segt[node] = segt[node*2];
-    else
-        segt[node] = segt[node*2+1];
+    ll mid = (b+e)>>1;
+    build(p<<1,b,mid);
+    build((p<<1)|1,mid+1,e);
+    push_up(p);
 }
-//将延迟标记向下传递
-void pushdown(int node){
-    if(addmark[node] != 0){
-	//因为可能存在被多个延迟标记标记的情况，所以这里使用+=
-        addmark[2*node] += addmark[node];
-        addmark[2*node+1] += addmark[node];
-	//当前节点的标记的意义是，当前节点的子节点需要更新标记记录的值，而和当前节点无关
-	//所以在将当前节点的标记清零时，要将子节点的值更新标记记录的值，而上面两行是记录子节点的子节点需要更新的值
-        segt[2*node] += addmark[node];
-        segt[2*node+1] += addmark[node];
-        addmark[node] = 0;
-    }
+void push_down(ll p,ll b,ll e){
+    if(tag[p] == 0) return;
+    ll mid = (b+e)>>1;
+    segt[p<<1] += tag[p]*(mid-b+1);
+    tag[p<<1] += tag[p];
+    segt[(p<<1)|1] += tag[p]*(e-mid);
+    tag[(p<<1)|1] += tag[p];
+    tag[p] = 0;
 }
-int __query(int node,int qb,int qe,int nb,int ne){
-    if(qb > ne || qe < nb) return INF;
-    if(qb <= nb && ne <= qe) return segt[node];
-    pushdown(node);
-    int mid = nb+(ne-nb)/2;
-    return min(__query(node*2,qb,qe,nb,mid),__query(node*2+1,qb,qe,mid+1,ne));
-}
-//更新单个值
-void updateOne(int node,int index,int addval,int nb,int ne){
-    if(nb == ne){
-        segt[node] += addval;
+void update(ll l,ll r,ll b,ll e,ll p,ll k){
+    if(l <= b && e <= r){
+        tag[p] += k;		  //表示当前节点的子节点需要更新
+        segt[p] += (k*(e-b+1));　　｀//[b,e]的每一个元素都加k，所以节点就要加上k*(e-b+1)
         return;
     }
-    int mid = nb+(ne-nb)/2;
-    if(index <= mid) updateOne(node*2,index,addval,nb,mid);
-    else             updateOne(node*2+1,index,addval,mid+1,ne);
-    segt[node] = min(segt[node*2],segt[node*2+1]);
+    push_down(p,b,e);
+    ll mid = (b+e) >> 1;
+    if(l <= mid) update(l,r,b,mid,p<<1,k);
+    if(mid < r) update(l,r,mid+1,e,(p<<1)|1,k);
+    push_up(p);
 }
-//更新一个区间的所有值
-void updateInterval(int node,int ub,int ue,int addval,int nb,int ne){
-    if(ub > ne || ue < nb) return;
-    if(nb <= ub && ue <= ne) {
-        addmark[node] += addval;
-        segt[node] += addval;
-        return;
-    }
-    pushdown(node); //先更新子节点，再往下递归更新
-    int mid = nb+(ne-nb)/2;
-    updateInterval(node,ub,mid,addval,nb,ne);
-    updateInterval(node,mid+1,ue,addval,nb,ne);
-    //此时可以保证子节点的值已经更新，然后再更新当前节点的值
-    segt[node] = min(segt[2*node],segt[2*node+1]);
-}
-int main(){
-    srand(time(NULL));
-    for(int i = 1; i <= MAXN; i++){
-        arr[i] = rand()%200;
-        printf("%5d ",arr[i]);
-    }
-    cout << endl;
-    for(int i = 1; i <= MAXN; i++){
-        printf("%5d ",i);
-    }
-    cout << endl;
-    build(1,1,MAXN);
-    int T;
-    cin >> T;
-    while(T--){
-        int l,r;
-        cin >> l >> r;
-        int res = __query(1,l,r,1,MAXN);
-        cout << res << endl;
-        int index,addval;
-        cin >> index >> addval;
-        updateOne(1,index,addval,1,MAXN);
-        cin >> l >> r;
-        res = __query(1,l,r,1,MAXN);
-        cout << res << endl;
-    }
-    
+ll query(ll l,ll r,ll b,ll e,ll p){
+    ll res = 0;
+	//查询区间在当前节点区间内，则返回当前节点的值
+    if(l <= b && e <= r) return segt[p];
+    push_down(p,b,e);
+    ll mid = (b+e)>>1;
+    if(l <= mid) res += query(l,r,b,mid,p<<1);
+    if(mid < r) res += query(l,r,mid+1,e,(p<<1)|1);
+    return res;
 }
